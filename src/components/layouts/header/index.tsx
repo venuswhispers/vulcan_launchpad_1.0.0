d1ea580d23241840ec253046496a71f17996c086
venuswhispers <venuswhisperer412@gmail.com>
2024-04-03 06:16:56 +0800

    update: sider collapse and header mobile responsive

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -7,7 +7,7 @@ import { Dropdown } from "flowbite-react";
 const Header = () => {
 
   const _renderWalletConnectButton = () => (
-    <div className="flex gap-2 text-sm font-bold">
+    <div className="flex gap-1 sm:gap-2 text-sm font-bold">
       <div className="founded-full p-[5px] flex items-center dark:bg-black rounded-full dark:text-[#5D5F68] hover:dark:text-white cursor-pointer">
         <Image
           src={"/images/eth.png"}
@@ -34,7 +34,7 @@ const Header = () => {
   )
 
   return (
-    <div className="flex items-center flex-none gap-6 p-4 text-black dark:text-white w-full justify-between bg-white dark:bg-[#100E28] border-2 border-[#E3E3E3] dark:border-[#100E28] rounded-xl">
+    <div className="flex items-center flex-none gap-3 sm:gap-6 p-4 text-black dark:text-white w-full justify-between bg-white dark:bg-[#100E28] border-2 border-[#E3E3E3] dark:border-[#100E28] rounded-xl">
       
       <ul className="hidden sm:flex md:hidden lg:flex text-sm text-[#777E90] dark:text-white items-center gap-9">
         <li className="cursor-pointer hover:opacity-60 truncate">Discover</li>
@@ -44,10 +44,9 @@ const Header = () => {
         <Dropdown.Item>Discover</Dropdown.Item>
         <Dropdown.Item>How it work</Dropdown.Item>
       </Dropdown>
-      
 
-      <div className="flex gap-6 items-center">
-        <div className="relative">
+      <div className="flex gap-1 sm:gap-6 items-center">
+        <div className="relative mr-2">
           <Icon icon="ph:bell-thin" width={22}/>
           <div className="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-[#45B26B] border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900"></div>
         </div>
--- src/components/layouts/sider/index.tsx
+++ src/components/layouts/sider/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { setConfig } from "next/config";
 
 interface INav {
   title: string,
@@ -24,6 +25,7 @@ const Sider = () => {
 
   const { theme, setTheme } = useTheme();
   const [current, setCurrent] = React.useState<string>("Dashboard");
+  const [isCollapse, setIsCollapse] = React.useState<boolean>(true);
 
   const _renderThemeSwitch = () => (
     <div className="relative flex bg-[#EFF3FF] dark:bg-[#050606] rounded-xl w-full px-2 py-1 mt-4">
@@ -58,45 +60,54 @@ const Sider = () => {
     </li>
   )
 
+  const handleCollapse = () => {
+    setIsCollapse(prev => !prev);
+  }
+
   return (
-    <div className="hidden md:flex flex-none flex-col justify-between bg-white dark:bg-[#100E28] w-[252px] border-2 border-[#E3E3E3] dark:border-[#100E28] rounded-xl">
-      <div>
-        <div className="flex justify-center items-center border-b-2 border-[#E3E3E3] dark:border-[#100E28] p-6">
-          <Image
-            src={ theme !== "dark" ? "/images/logo.dark.svg" : "/images/logo.svg" }
-            width={110}
-            height={110}
-            alt={"sun"}   
-            priority={true}    
-          />
-        </div>
+    <>
+      { !isCollapse && <div onClick={handleCollapse} className="fixed md:hidden top-0 left-0 right-0 bottom-0 backdrop-filter backdrop-blur-[10px]"></div> }
+      <div className={`fixed ${isCollapse ? '-left-[252px]' : 'left-4'} transition-all duration-200 bottom-4 z-10 top-4 md:static md:flex md:overflow-auto flex-none flex-col justify-between bg-white dark:bg-[#100E28] w-[252px] border-2 border-[#E3E3E3] dark:border-[#100E28] rounded-xl sider`}>
+        <div className="w-full h-full overflow-y-scroll sider">
+          <div>
+            <div className="flex justify-center items-center border-b-2 border-[#E3E3E3] dark:border-[#100E28] p-6">
+              <Image
+                src={ theme !== "dark" ? "/images/logo.dark.svg" : "/images/logo.svg" }
+                width={110}
+                height={110}
+                alt={"sun"}   
+                priority={true}    
+              />
+            </div>
+            <div className="flex justify-center items-center flex-col mt-4">
+              <Image
+                src="/images/man.png"
+                width={70}
+                height={70}
+                alt={"avatar"}  
+                className="rounded-3xl" 
+                priority={true}    
+              />
+              <h3 className="text-black font-sans dark:text-white font-bold mt-3 text-lg">Erfan Amade</h3>
+              <div className="text-sm text mt-1 flex items-center gap-1"><span>verified</span><Icon className="text-[#0CAF60] text-[18px]" icon="ic:baseline-verified" /></div>
+            </div>
 
-        <div className="flex justify-center items-center flex-col mt-4">
-          <Image
-            src="/images/man.png"
-            width={70}
-            height={70}
-            alt={"avatar"}  
-            className="rounded-3xl" 
-            priority={true}    
-          />
-          <h3 className="text-black font-sans dark:text-white font-bold mt-3 text-lg">Erfan Amade</h3>
-          <div className="text-sm text mt-1 flex items-center gap-1"><span>verified</span><Icon className="text-[#0CAF60] text-[18px]" icon="ic:baseline-verified" /></div>
+            <ul className="mt-4 p-6">
+              { navs.map((_nav: INav) => _renderNavItem(_nav)) }
+            </ul>
+          </div>
+          <div className="p-6">
+            { _renderThemeSwitch() }
+            <h2 className="text mt-2 px-2">Insigts</h2>
+            <div className="flex justify-between items-center mt-3 px-1">
+              <div className="text flex gap-2 items-center"><Icon icon="tabler:message-circle" width={30} hFlip/><span>Inbox</span></div>
+              <div className="p-1 px-2 rounded-md text-white bg-[#FF3E46]">8</div>
+            </div>
+          </div>
         </div>
-
-        <ul className="mt-4 p-6">
-          { navs.map((_nav: INav) => _renderNavItem(_nav)) }
-        </ul>
+        <Icon onClick={handleCollapse} icon="material-symbols:arrow-forward-ios" width={30} className={`absolute cursor-pointer hover:opacity-60 right-0 top-1/2 translate-x-full -translate-y-1/2 !z-50 md:hidden dark:text-white ${!isCollapse && 'hidden'}`}/>
       </div>
-      <div className="p-6">
-        { _renderThemeSwitch() }
-        <h2 className="text mt-2 px-2">Insigts</h2>
-        <div className="flex justify-between items-center mt-3 px-1">
-          <div className="text flex gap-2 items-center"><Icon icon="tabler:message-circle" width={30} hFlip/><span>Inbox</span></div>
-          <div className="p-1 px-2 rounded-md text-white bg-[#FF3E46]">8</div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
