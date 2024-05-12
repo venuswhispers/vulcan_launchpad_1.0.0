4106f127450cf18e640d7a5001fd3273cb97573d
venuswhispers <venuswhisperer412@gmail.com>
2024-05-12 10:35:17 +0800

    update: token

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,6 +1,4 @@
-"use client";
 import React from "react";
-import { type Chain, type Address, type Client } from "viem";
 import { useSignMessage } from "wagmi";
 import useActiveWeb3 from "@/hooks/useActiveWeb3";
 import axios from 'axios';
